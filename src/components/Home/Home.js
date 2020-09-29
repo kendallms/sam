c5246da10ec498d21fab105bf8c00c9777987b48
Kendall Moffett-Sklaroff <kmoffettsklaroff@deloitte.com>
2020-09-29 14:37:48 -0400

    removed image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
        <h1>
           I'm {name}
         </h1>
-        <img src={require('./../../sam.png')} width='300'></img> 
+        {/* <img src={require('./../../sam.png')} width='300'></img>  */}
         <p className='bio'>Website coming soon!</p>
         {/* <p className='bio'>Born and raised in the DMV area, I'm a developer with a ton of experience in the health field. I've worked for the past seven years doing clinical trial research. Along the way, I noticed that healthcare softcare can be prtty rough. I sought out to first learn the design aspect of it, and then the functionality. I'm routinely fascinated by new systems of technology, and am constanting thinking about the future state of hunans.</p> */}
       
